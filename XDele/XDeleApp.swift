30bca54d273864d8c999fedefd5664683fbfe64d
@IAmNathanPalmer <@IAmNathanPalmer>
2025-08-31 22:07:50 -0500

    Initial Commit

---
+++ XDele/XDeleApp.swift
@@ -0,0 +1,17 @@
+//
+//  XDeleApp.swift
+//  XDele
+//
+//  Created by Bayley Hornbuckle on 8/31/25.
+//
+
+import SwiftUI
+
+@main
+struct XDeleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ XDeleTests/XDeleTests.swift
@@ -0,0 +1,17 @@
+//
+//  XDeleTests.swift
+//  XDeleTests
+//
+//  Created by Bayley Hornbuckle on 8/31/25.
+//
+
+import Testing
+@testable import XDele
+
+struct XDeleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
